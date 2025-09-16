a638f58677959af64af5ea98216358a3ae51b101
Francisco Quinones <fquinonescedeno@usf.edu>
2025-09-16 16:51:01 -0400

    Step 2

--- cc_4.js
+++ cc_4.js
@@ -1,3 +1,40 @@
 // Coding Challenge 04a
 
 // Code goes here
+
+// Step 2 Initial Array
+
+let Products = [
+    {
+        Name: "Watch"
+        Category: "Electronics"
+        Price: 59.99
+        Inventory: 4
+    },
+    {
+        Name: "Peppers"
+        Category: "Groceries"
+        Price: 4.99
+        Inventory: 10
+    },
+    {
+        Name: "Clorox Wipes"
+        Category: "Household"
+        Price: 9.99
+        Inventory: 3
+    },
+    {
+        Name: "Black T-Shirt"
+        Category: "Apparel"
+        Price: 19.99
+        Inventory: 9
+    },
+    {
+        Name: "Nike Shoes"
+        Category: "Apparel"
+        Price: 49.99
+        Inventory: 6
+    },
+];
+
+
