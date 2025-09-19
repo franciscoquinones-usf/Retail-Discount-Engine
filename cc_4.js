0eda91f59a382362ec01f5bb83bf8cf711655f6a
Francisco Quinones <fquinonescedeno@usf.edu>
2025-09-19 15:13:36 -0400

    Step 5

--- cc_4.js
+++ cc_4.js
@@ -36,7 +36,7 @@ for (let product of Products) {
 product.Price = parseFloat((product.Price * (1-discount)).toFixed(2));
 }
 
-// Step 4: New Variables with discount. 
+// Step 4: New Variables with discount 
 
 let customerType = ["Regular", "Student", "Senior"];
 let total = 0;
@@ -56,3 +56,38 @@ if (customerType === "Student") {
 }
 
 let finalTotal = parseFloat((total * (1-extraDiscount)).toFixed(2));
+
+// Step 5: For loops as checkout process 
+
+let customers = [
+    {name: "Jane", type: "Regular", shoppingList: ["Watch", "Black T-Shirt", "Nike Shoes"]},
+    {name: "John", type: "Student", shoppingList: ["Watch", "Peppers", "Black T-Shirt"]},
+    {name: "Erick", type: "Senior", shoppingList: ["Peppers", "Clorox Wipes"]},
+];
+
+for (let i = 0; i < customers.length; i++) {
+    let customer = customers[i];
+    let cartTotal = 0;
+
+for (let item of customer.shoppingList) {
+    let product = Products.find(p => p.Name === item);
+
+    if (product && product.Inventory > 0) {
+        cartTotal += product.Price;
+        product.Inventory--;
+    } else {
+        console.log(`${customer.name} attemped to purchase ${item}, but it's out of stock`);
+    }
+    }
+
+let extraDiscount = 0;
+if (customer === "Student") {
+    extraDiscount = 0.05;
+} else if (customer === "Senior") {
+    extraDiscount = 0.07;
+}
+
+let finalTotal = parseFloat((cartTotal * (1-extraDiscount)).toFixed(2));
+}
+
+// Step 6
